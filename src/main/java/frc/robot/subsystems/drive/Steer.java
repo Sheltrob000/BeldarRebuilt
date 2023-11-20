9a5881295c145633dfb7ae63329cb8869793d141
sheltrob000 <sheltrob000@warren.k12.in.us>
2023-11-20 11:00:55 -0500

    some steer constants

--- src/main/java/frc/robot/subsystems/drive/Steer.java
+++ src/main/java/frc/robot/subsystems/drive/Steer.java
@@ -2,24 +2,31 @@ package frc.robot.subsystems.drive;
 
 public abstract class Steer {
     protected static final class Constants {
-        // TODO: create a double called dtSeconds and initialize to 0.020
-        // TODO: create a double called gearing and initialize to -150.0 / 7.0
-        // TODO: create a double called kV and initialize to 0.427607143
-        // TODO: create a double called kA and initialize to 0.005493643
-        // TODO: create a double called maxPositionErrorRadians and initialize to 0.125
-        // TODO: create a double called maxVelocityErrorRadiansPerSec and initialize to
-        // 1
+        private static final double dtSeconds = 0.020;
+        private static final double gearing = -150.0 / 7.0;
+        private static final double KV = 0.427607143;
+        private static final double KA = 0.005493643;
+        private static final double maxPositionErrorRadians = 0.125;
+        private static final double maxVelocityErrorRadiansPerSec = 1;
         // TODO: create a LinearSystem<N2, N1, N1> called plant and initialize to
         // LinearSystemId.identifyPositionSystem(kV, kA)
+
+
         // TODO: create a Vector<N2> called qelms and initialize to
         // VecBuilder.fill(maxPositionErrorRadians, maxVelocityErrorRadiansPerSec)
+        
         // TODO: create a Vector<N1> called relms and initialize to
         // VecBuilder.fill(RobotController.getBatteryVoltage())
+
         // TODO: create a LinearQuadraticRegular<N2, N1, N1> called controller and
         // initialize with appropriate constants
+
         // TODO: create a double called kP initialize to controller.getK().get(0, 0)
+
         // TODO: create a double called kI and initialize to 0.0
+
         // TODO: create a double called kD and initialize to controller.getK().get(0, 1)
+        
         // TODO: create a double called tolerance and initialize to Math.abs(0.0239 * 2
         // * Math.PI / Constants.gearing)
     }
