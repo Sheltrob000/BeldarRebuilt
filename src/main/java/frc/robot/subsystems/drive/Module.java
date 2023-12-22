b6242cb500e7ef57f15409d96b7ddf0a1ddb3536
sheltrob000 <sheltrob000@warren.k12.in.us>
2023-12-22 09:55:17 -0500

    the module is done

--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -48,60 +48,43 @@ public class Module {
     }
 
     public SwerveModuleState getSwerveModuleState() {
-        // TODO: create a double called velocityMetersPerSecond and initialize from
-        // wheel's
-        // getVelocityMetersPerSecond() method
-        // TODO: create a double called positionDegrees and initialize from steer's
-        // getPositionDegrees() method
-        // TODO: create a Rotation2d object called angle and intialize using
-        // Rotation2d's static fromDegrees() method.
-        // TODO: create a SwerveModuleState object called swerveDriveState and
-        // intialize using velocityMetersPerSecond and angle
-        // TODO: return SwerveModuleState
-        return null; // TODO: remove this line when done.
+        double velocityMetersPerSecond = wheel.getVelocityMetersPerSecond();
+        double positionDegrees = steer.getPositionDegrees();
+        Rotation2d angle = Rotation2d.fromDegrees(positionDegrees);
+        SwerveModuleState swerveModuleState = new SwerveModuleState(velocityMetersPerSecond, angle);
+        return swerveModuleState;
     }
 
     public double getAbsoluteAngleEncoderDegrees() {
         // return absoluteAngles angle in degrees
-        return 0.0; // TODO: remove this line when done.
+        return absAngleEncoder.getAbsoluteAngle().getDegrees();
     }
 
     public boolean isAbsoluteEncoderDataGood() {
-        // this is here to DriveSubsystem can put this on the dashboard for testing.
-        // return absAngleEncoder's isDataGood method result
-        return false; // TODO: return this line when done.
+        return absAngleEncoder.isDataGood();
     }
 
-    public void setSwerveModuleAngle(double degrees) {
-        // TODO: FREEBEE. Just comment out.
-        // basically this checks if absoluteEncoder data is good
-        // sets it to it if so.
-        // else sets its to 0 assuming we manually turned the module.
-        // if (isAbsoluteEncoderDataGood()) {
-        // double degrees = getAbsoluteAngleEncoderDegrees();
-        // steer.setPositionDegrees(degrees);
-        // } else {
-        // System.out.println("Bad AbsoluteEncoderData for Module " + moduleNumber + " :
-        // Assume manually set to 0.0");
-        // steer.setPositionDegrees(0.0);
-        // }
+    public void setSwerveModuleAngle() {
+        if (isAbsoluteEncoderDataGood()) {
+        double degrees = getAbsoluteAngleEncoderDegrees();
+        steer.setPositionDegrees(degrees);
+        } else {
+        System.out.println("Bad AbsoluteEncoderData for Module " + moduleNumber + " : Assume manually set to 0.0");
+        steer.setPositionDegrees(0.0);
+        }
     }
 
     public void controlModule(double degrees, double metersPerSecond) {
-        // TODO: turn the module using steer's turnToPosition method passing in degrees
-        // TODO: drive the wheel using the wheel's driveAtVelocity method passing in
-        // metersPerSecond
+        steer.setPositionDegrees(degrees);
+        wheel.driveAtVelocity(metersPerSecond);
     }
 
     public void controlModule(double degrees) {
-        // TODO: NOTE this method is just a convenience feature.
-        // TODO: Call controlModule pass in the degrees parameter and 0.0;
+        controlModule(degrees, 0.0);
     }
 
     public void stop() {
-        // TODO: NOTE another convenience feature. Applies zero voltage to both the
-        // steer and the wheel
-        // TODO: use steer's setInputVoltage and set to 0.0
-        // TODO: use the wheel's setInputVoltage and set to 0.0
+        steer.setInputVoltage(0.0);
+        wheel.setInputVoltage(0.0);
     }
 }
